0210d43866af958cfe482180d2c973c0f23f0c90
Anthony Babka <anthonybabka@gmail.com>
2022-06-02 11:46:09 -0700

    completed part 11, has a few bugs to fix

--- views/default.jsx
+++ views/default.jsx
@@ -1,25 +1,31 @@
-const React = require('react')
+const React = require('react');
 
 function Def (html) {
     return (
         <html>
             <head>
-                <title>Title</title>
-                <link rel="stylesheet" href="/css/style.css"/>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
+            <title>{html.title='Rest-Rant'}</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
+            <link href="/css/styles.css" rel="stylesheet"></link>
             </head>
             <body>
-                <nav>
-                    <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/places">Places</a></li>
-                        <li><a href="/places/new">Add Place</a></li>
-                    </ul>
-                </nav>
-                {html.children}
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/places">Places</a>
+                    </li>
+                    <li>
+                        <a href="/places/new">Add Place</a>
+                    </li>
+                </ul>
+            </nav>
+            {html.children}
             </body>
         </html>
     )
 }
 
-module.exports = Def
+module.exports = Def;
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,33 +1,114 @@
-const React = require('react')
-const Def = require('../default')
+const React = require('react');
+const Def = require('../default');
 
-function new_form (data) {
-  let message = ''               
-    if (data.message) {
-      message = (
-        <h4 className="alert-danger">
-          {data.message}
-        </h4>,
-      {message}
+function show (data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    let rating = (
+        <h3 className="inactive">
+          Not yet rated
+        </h3>
       )
-    }
+      if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+          return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ""
+        for(let i = 0; i < averageRating; i++) {
+            stars += "⭐"
+        }
+        rating = (
+          <h3>
+             { stars } stars
+          </h3>
+        )
+          comments = data.place.comments.map(c => {
+            return (
+              <div className="border">
+                <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+                <h4>{c.content}</h4>
+                <h3>
+                  <stong>- {c.author}</stong>
+                </h3>
+                <h4>Rating: {c.stars}</h4>
+                <form method="POST" action = {`/places/${data.place.id}/comment/${c.id}?_method=DELETE`}>
+                    <input type="submit" className='btn btn-danger' value="Delete Comment"></input>
+                </form>
+              </div>
+            )
+          })
+        }
     return (
-      <Def>
-      <main>
-        <div className="row">
-          <div className="col-sm-6">
-            <img src={data.place.pic} alt={data.place.name} />
-            <h3>Located in {data.place.city}, {data.place.state}</h3>
-          </div>
-          <div className="col-sm-6">
-            <h2>Description</h2>
-            <h3>{data.place.showEstablished()}</h3>
-            <h4>Serving {data.place.cuisines}</h4>
-          </div>
-        </div>
-      </main>
-    </Def>
+        <Def>
+            <br></br>
+            <main>
+                <h1>{ data.place.name }</h1>
+                <br></br>
+                <div className="rating">
+                    <h2>{ rating }</h2>
+                    currently unrated...
+                </div>
+                <br></br>
+                <div className="images">
+                    <img src={data.place.pic} alt={data.place.name}></img>
+                </div>
+                <div className="description">
+                <br></br>
+                    <h3>Located in {data.place.city}, {data.place.state}</h3>
+                    Founded in year {data.place.founded}
+                    <br></br>
+                    <h3>{data.place.showEstablished()}</h3>
+                    <br></br>
+                    <h4>Serving {data.place.cuisines}</h4>
+                </div>
+                <br></br>
+                <div className="comments">
+                    <h2>Comments</h2>
+                    { comments }
+                </div>
+                <form method='POST' action={`/places/${data.place.id}/comment/`}>
+
+                <div className='form-group'>
+                    <label htmlFor='author'>Author</label>
+                    <input className='form-control' id='author' name='author'/>
+                </div>
+
+                <div className='form-group'>
+                    <label htmlFor='content'>Content</label>
+                    <input className='form-control' id='content' name='content' type='textarea' />
+                </div>
+
+                <div className='form-group'>
+                    <label htmlFor='stars' class="form-label">Star Rating</label>
+                    <input className='form-control' class="rating" id='stars' name='stars' type='range' step='0.5' min='0' max='5' />
+                </div>
+
+                <div className='form-group'>
+                    <label htmlFor='rant'>Rant</label>
+                    <input  id='rant' name='rant' type='checkbox' defaultChecked/>
+                </div>
+
+                <input className='btn btn-primary' type='submit' value='Add Comment' />
+                </form>
+
+                <br></br>
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                    Edit
+                </a>
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <br></br>
+                    <button type="submit" className="btn btn-danger">
+                        Delete
+                    </button>
+                </form>
+                <br></br>
+            </main>
+        </Def>
     )
 }
 
-module.exports = show
+module.exports = show;
